1bddf7f2c32aa709498759f596ff6b7ef101df2c
Gustavo loyola <guh.loyola@gmail.com>
2024-11-06 10:15:42 -0300

    Exemplo de componente de classe adicionado a pasta

---
+++ src/components/Introducao/componenteClasse.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react'
+
+export default class componenteClasse extends Component {
+    render() {
+        return (
+            <h1>Esse é um componente de Classe</h1>
+        )
+    }
+}
