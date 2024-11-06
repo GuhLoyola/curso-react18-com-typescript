43c4c473a602f20816d3a29c45b8b2b0e03b651b
Gustavo loyola <guh.loyola@gmail.com>
2024-11-06 13:56:10 -0300

    Componente equivalente ao módulo de eventos adicionado ao app

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
-// Exemplo de um componente de CLASSE:
-
-import Card from "./components/Card"
-import User from "./components/User"
-
-/*
-
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    return <h1>Esse é um componente de classe</h1>
-  }
-}
-
-*/
-
-// Exemplo de um componente de FUNÇÃO: 
+import './App.css'
+import EventoPorProp from './components/EventosNoReact/eventoPorProp'
 
 const App = () => {
 
   return (
-    <>
-      <h1>
-        Meu primeiro componente de usuário
-      </h1>
-
-      <Card>
-        <h2>Olá</h2>
-        <User name="Gustavo" age={21} email="gustavo.loyola@gmail.com" role={[{ id: 1, job: 'CEO' }]} />
-      </Card>
-
-
-      {/* Também da pra usar o operador ternário */}
-    </>
+    <div>
+      <EventoPorProp />
+    </div>
   )
 
 
